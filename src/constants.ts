24ca0e5a1bc78d0facd4924cd07cf0f32d3d04e8
welvet@gmail.com <welvet@gmail.com>
2025-09-17 21:20:07 +0200

    fix spelling

--- src/constants.ts
+++ src/constants.ts
@@ -116,5 +116,5 @@ export const DEFAULT_TASK_DESCRIPTION = `## Your Role as Narrator
 Generate 6 possible next actions for the current scene.
 {{/if}}
 
-Always respond in valid XML format, as shown in the example below.
+Always respond in valid XML format, as shown in the example above.
 `;
